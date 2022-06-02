1a97e017ea23e4f354e6b01154171f17901f334e
stefan-ysh <58702199+stefan-ysh@users.noreply.github.com>
2022-06-03 01:37:44 +0800

    Update index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,7 +6,7 @@ export const uuid = (hasHyphen?: string) => {
       : "xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx"
   ).replace(/[xy]/g, function (c) {
     const r = (Math.random() * 16) | 0;
-    const v = c == "x" ? r : (r & 0x3) | 0x8;
+    const v = c === "x" ? r : (r & 0x3) | 0x8;
     return v.toString(16);
   });
 };
