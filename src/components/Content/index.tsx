c5809adedba560df420ca974c0bed256c09cba02
stefan-ysh <58702199+stefan-ysh@users.noreply.github.com>
2022-06-02 22:06:55 +0800

    opt：默认首页为 todolist

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -9,7 +9,8 @@ function Content() {
     <div className="content">
       <Switch>
         <Route exact path={"/index"} component={Index}></Route>
-        <Route exact path={"/todo-list"} component={TodoList}></Route>
+        {/* <Route exact path={"/todo-list"} component={TodoList}></Route> */}
+        <Route exact path={"/"} component={TodoList}></Route>
       </Switch>
     </div>
   );
