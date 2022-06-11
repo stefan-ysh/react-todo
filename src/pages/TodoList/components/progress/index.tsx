045f6fbc658dd218d3abb188a3bea05bd1e6d525
stefan-ysh <58702199+stefan-ysh@users.noreply.github.com>
2022-06-11 21:10:16 +0800

    Update index.tsx

--- src/pages/TodoList/components/progress/index.tsx
+++ src/pages/TodoList/components/progress/index.tsx
@@ -3,9 +3,9 @@ import { Progress } from "antd";
 import moment from "moment";
 function TaskProgress(props: any) {
   const { startTime, planStartTime, planEndTime } = props.task;
-  let max = moment(planEndTime).valueOf() - moment(planStartTime).valueOf();
+  const max = moment(planEndTime).valueOf() - moment(planStartTime).valueOf();
   const val = moment().valueOf() - moment(startTime).valueOf();
-  let percent = Number((val / max).toFixed(2)) * 100;
+  const percent = Number((val / max).toFixed(2)) * 100;
   return (
     <div className="progress-wrap">
       <Progress
